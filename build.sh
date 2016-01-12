0e00f96a8a6cc3cbf67d251bf815b0c585c52adc
Andy Walker <walkeraj@gmail.com>
2016-01-12 00:28:13 -0500

    use -p <viewer binary> to preview pdf after build

--- build.sh
+++ build.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+OPTIND=1
 pdflatex golang-ref-sheet.tex
-rm golang-ref-sheet.aux
-rm golang-ref-sheet.log
+rm -f golang-ref-sheet.aux
+rm -f golang-ref-sheet.log
+rm -f figures/*converted-to.pdf
+while getopts ":p:" opt; do
+    case "$opt" in
+        p)
+            $OPTARG golang-ref-sheet.pdf
+            ;;
+        :)
+            echo "-$OPTARG requires an argument." >&2
+            exit 1
+            ;;
+    esac
+done
