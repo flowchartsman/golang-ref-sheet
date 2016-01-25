a74981eb5e4aaeca8e5b6636be380d6084e7ee1d
Andy Walker <walkeraj@gmail.com>
2016-01-25 17:28:11 -0500

    larger preview size

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ OPTIND=1
 export BUILD_DIR=${BUILD_DIR-${TMPDIR}}
 
 generate_preview () {
-    convert -density 250 -background white -alpha remove golang-ref-sheet.pdf[0] -crop 600x400+$1+$2 \( +clone -background black -shadow 80x3+4+4 \) +swap -background white -layers merge +repage github/$3
+    convert -density 250 -background white -alpha remove golang-ref-sheet.pdf[0] -crop 600x600+$1+$2 \( +clone -background black -shadow 80x3+4+4 \) +swap -background white -layers merge +repage github/$3
 }
 
 lualatex --shell-escape --output-directory=$BUILD_DIR -halt-on-error golang-ref-sheet.tex
